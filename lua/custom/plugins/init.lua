0137913140b5ceeb10a5510ff3055163d90ea6e2
jun.dai <jun.dai@denglin.ai>
2024-05-24 14:09:58 +0800

    [add] add project.nvim to auto find root dir, auto open nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,6 +25,27 @@ return {
           dotfiles = true,
         },
       }
+      local api = require 'nvim-tree.api'
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc }
+      end
+
+      vim.keymap.set('n', '<leader>stc', function()
+        api.tree.find_file { buf = vim.fn.expand '%:p', open = true, focus = true }
+      end, opts '[L]ocate [F]ile in nvim-tree')
+    end,
+  },
+  {
+    'ahmedkhalf/project.nvim',
+    config = function()
+      require('project_nvim').setup {
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+      }
     end,
   },
 }
